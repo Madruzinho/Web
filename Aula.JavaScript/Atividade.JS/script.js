92ab0b69cea13757ff07cf95eae530e4b7b8d29c
Madruzinho <gui.augusto.pessoal@gmail.com>
2025-09-25 23:22:16 -0300

    Colocando a mudança de cor do fundo

--- Aula.JavaScript/Atividade.JS/script.js
+++ Aula.JavaScript/Atividade.JS/script.js
@@ -7,16 +7,22 @@ function calcular(opcao) {
 
     if(opcao == 1) {
         resultado = num1 + num2;
+        document.body.style.backgroundColor = "lightgreen";
     } else if (opcao == 2) {
         resultado = num1 - num2;
+        document.body.style.backgroundColor = "lightblue";
     } else if (opcao == 3) {
         resultado = num1 * num2;
+        document.body.style.backgroundColor = "orange";
     } else if (opcao == 4) {
         if (num2 == 0) {
+            document.body.style.backgroundColor = "red";
             alert("não é possivel divisao por zero")
-            document.getElementById("resultado").textContent = "Resultado: erro";
-        }
+            return document.getElementById("resultado").textContent = "Resultado: erro";
+        } else {
         resultado = num1 / num2;
+        document.body.style.backgroundColor = "purple";
+        }
     }
 
     return document.getElementById("resultado").textContent = "Resultado: " + resultado;
