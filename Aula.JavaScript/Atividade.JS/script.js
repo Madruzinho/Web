06156a79fe01800b6a4116873503adf3c0d28d85
Madruzinho <gui.augusto.pessoal@gmail.com>
2025-09-25 23:15:59 -0300

    Enviando atividade Calculadora JS

--- Aula.JavaScript/Atividade.JS/script.js
+++ Aula.JavaScript/Atividade.JS/script.js
@@ -1,7 +1,10 @@
-const readline = require("readline-sync");
 
-function calcular(num1, num2, opcao) {
+
+function calcular(opcao) {
     let resultado = 0;
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+
     if(opcao == 1) {
         resultado = num1 + num2;
     } else if (opcao == 2) {
@@ -9,9 +12,12 @@ function calcular(num1, num2, opcao) {
     } else if (opcao == 3) {
         resultado = num1 * num2;
     } else if (opcao == 4) {
+        if (num2 == 0) {
+            alert("não é possivel divisao por zero")
+            document.getElementById("resultado").textContent = "Resultado: erro";
+        }
         resultado = num1 / num2;
     }
+
     return document.getElementById("resultado").textContent = "Resultado: " + resultado;
 }
-
-calcular(5, 10, 3);
